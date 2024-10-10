8577c4bf8c195a9134dbc576572e1f756d9de742
DESKTOP-V67L6O2\User <angelaplaton1989@gmail.com>
2024-10-10 11:00:03 +0800

    no message

---
+++ src/activityMD01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class activityMD01 {
+   public static void main(String[] args){
+       //Declare and Initialize
+       char [][] arr = new char [3][4];
+       
+       //fill the array
+       for(int i = 0; i < arr.length; i++){
+           for(int j = 0; j < arr[i].length; j++){
+               arr[i][j] = 'o'; 
+           }
+       }
+   }
+}
